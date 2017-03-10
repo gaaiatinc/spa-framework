7055739001b23a826cdf511624b3f119379ed531
Ali Ismael <aismael@paypal.com>
2017-03-10 01:18:06 -0800

    Added runningInBrowser() utility method

--- libraryFacade.js
+++ libraryFacade.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import {apiGet, apiPost} from "./lib/actions/rest_actions";
+import {apiGet, apiPost, runningInBrowser} from "./lib/actions/rest_actions";
 import {SPATemplate} from "./lib/SPATemplate";
 
-export {SPATemplate, apiGet, apiPost};
+export {SPATemplate, apiGet, apiPost, runningInBrowser};
